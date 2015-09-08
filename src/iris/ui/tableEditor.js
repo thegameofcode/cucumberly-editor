ba08b6bb0daa6d92c95e79d2b82ec4ef712af9e6
Angel Sanchez <angel.sanchez@intelygenz.com>
2015-09-08 13:38:04 +0200

    added option to remove rows and columns

--- src/iris/ui/tableEditor.js
+++ src/iris/ui/tableEditor.js
@@ -34,25 +34,59 @@ iris.ui(function (self) {
 	function addRow(text) {
 		var $tr = $('<tr>');
 		self.get('tableBody').append($tr);
-		for (var i = 0; i < numCols; i++) {
-			var params = ['Row value', $tr];
+		for (var i = 0; i < numCols+1; i++) {
+			if (i == 0) {
 
-			if (text) {
-				params.push(text[i]);
+				var $td = $('<td class="text-center" style="width: 30px;"></td>').appendTo($tr);
+
+				// skip first row
+				if ($tr.index() !== 0) {
+					var $btnRemoveRow = $('<a data-id="btnRemove" class="btn btn-flat ink-reaction"><i class="fa fa-trash"></i></a>').appendTo($td);
+					$btnRemoveRow.on('click', onRemoveRow);
+				}
+
+			} else {
+				var params = ['Row value', $tr];
+
+				if (text) {
+					params.push(text[i]);
+				}
+
+				addEditableCell.apply(this, params);
 			}
-
-			addEditableCell.apply(this, params);
 		}
 	}
 
 	function addCol(text) {
 		numCols++;
-		addEditableCell('Column name', self.get('tableHead').find('tr'), text);
+		var $container = addEditableCell('Column name', self.get('tableHead').find('tr'), text);
+
+		if (numCols > 1) {
+			$container.append($('<a data-id="btnRemove" class="btn btn-flat ink-reaction stick-top-right"><i class="fa fa-trash"></i></a>').on('click', onRemoveCol));
+			$container.attr('data-col', numCols);
+		}
+	}
+
+	function onRemoveCol(e) {
+		var colIdx = $(e.currentTarget).parent().prop('cellIndex') + 1;
+
+		// TODO memory leaks with UI references?
+		self.get().find('tr td:nth-child(' + colIdx + ')').each(function() {
+			$(this).remove();
+		});
+
+		self.notify('change');
+	}
+
+	function onRemoveRow(e) {
+		// TODO memory leaks with UI references?
+		$(e.currentTarget).closest('tr').remove();
+		self.notify('change');
 	}
 
 	function addEditableCell(defaultText, parent, text) {
 		var rowId = 'row_' + cucumberly.util.randomId();
-		var $container = $('<td><div data-id="' + rowId + '"></div></td>');
+		var $container = $('<td style="position:relative;"><div data-id="' + rowId + '"></div></td>');
 		$container.appendTo(parent);
 
 		var editableText = self.ui(rowId, iris.path.ui.editableLabel.js, {defaultText: defaultText});
@@ -62,6 +96,8 @@ iris.ui(function (self) {
 		}
 
 		editableText.on('change', onCellChange);
+
+		return $container;
 	}
 
 	function onCellChange() {
