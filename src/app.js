b6b17cd0a8ae5fac3db3df9eae78cbeb71f8d23e
guumaster <gustavoandresmarin@gmail.com>
2015-09-19 18:59:50 +0200

    switched to electron and eslint

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import app from 'app';
+import BrowserWindow from 'browser-window';
+import path from 'path';
+
+const INDEX_HTML = path.join(process.cwd(), 'dist/index.html');
+const WINDOW_OPTS = {
+  title: 'Cucumberly Editor',
+  width: 1400,
+  height: 700,
+  'min-width': 1000,
+  'min-height': 600,
+  center: true
+};
+
+export default () => {
+  const debug = process.env.NODE_ENV !== 'production';
+
+  let mainWindow = null;
+
+  app.on('ready', () => {
+    mainWindow = new BrowserWindow(WINDOW_OPTS);
+
+    mainWindow.loadUrl(`file://${INDEX_HTML}`);
+    mainWindow.show();
+
+    if (debug) {
+      mainWindow.openDevTools({/* detach:true */});
+    }
+
+    mainWindow.on('closed', () => {
+      mainWindow = null;
+    });
+  });
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+};
+
