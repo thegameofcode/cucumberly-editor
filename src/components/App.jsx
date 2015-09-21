b5b2370f7ac1f06aabb29412f7c8ecad5bc38b2c
Angel Sanchez <angel.sanchez@intelygenz.com>
2015-09-21 15:56:38 +0200

    added modal to save version, added a manager that parses features into *.feature files, updated react bootstrap

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Router from 'react-router';
 
+import bookManager from '../manager/book';
+
 import Editor from './editor/Editor';
 import Feature from './editor/Feature';
 import Subjects from './subjects/Subjects';
 import BookStore from '../stores/BookStore';
 import BookActions from '../actions/BookActions';
 
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Button, Navbar, Nav, NavItem, Modal } from 'react-bootstrap';
 
 let { Route, RouteHandler, DefaultRoute } = Router;
 
@@ -16,22 +18,43 @@ class App extends Component {
     super(props);
 
     this.state = BookStore.getState();
-    console.log('App state', this.state)
+    this.state.showSaveModal = false;
   }
 
   onBookStoreChange(state) {
-    console.log('onBookStoreChange', state)
     this.setState(state);
   }
 
   componentDidMount() {
-    BookStore.listen(this.onBookStoreChange.bind(this));
+    this.refs.dirChooser.getDOMNode().setAttribute('nwdirectory', true);
 
+    BookStore.listen(this.onBookStoreChange.bind(this));
     BookActions.fetchBook();
   }
 
   componentWillUnmount() {
     BookStore.unlisten(this.onBookStoreChange.bind(this));
+  }
+
+  saveBookChanges() {
+    let dirChooser = this.refs.dirChooser.getDOMNode();
+    dirChooser.value = null;
+    dirChooser.dispatchEvent(new Event('click'));
+  }
+
+  hideSaveModal() {
+    this.setState({showSaveModal: false});
+  }
+
+  showSaveModal() {
+    this.setState({showSaveModal: true});
+  }
+
+  onDirSelected() {
+    this.hideSaveModal();
+
+    let dirPath = this.refs.dirChooser.getDOMNode().value;
+    bookManager.saveBook(dirPath, this.state.book);
   }
 
   render() {
@@ -46,8 +69,29 @@ class App extends Component {
             <NavItem eventKey={1} href='#/editor'>Editor</NavItem>
             <NavItem eventKey={3} disabled>Subjects</NavItem>
           </Nav>
+          <Nav right>
+            <NavItem onClick={this.showSaveModal.bind(this)}>Save version</NavItem>
+          </Nav>
         </Navbar>
         <RouteHandler book={this.state.book} />
+
+        <Modal show={this.state.showSaveModal} onHide={this.hideSaveModal.bind(this)}>
+          <Modal.Header>
+            <Modal.Title>Save version</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            Do you really want save the changes as a new version?
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={this.hideSaveModal.bind(this)}>Close</Button>
+            <Button bsStyle="primary" onClick={this.saveBookChanges.bind(this)}>Save changes</Button>
+          </Modal.Footer>
+
+        </Modal>
+
+        <input ref='dirChooser' onChange={this.onDirSelected.bind(this)} style={{display: 'none'}} type='file' />
       </div>
     );
   }
